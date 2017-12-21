e6d43358d7db1c0317f1ac65a885c6531c4223f6
Parth <ppmistry204@gmail.com>
2017-12-21 16:47:25 +0530

    Addin crossorigin request.

--- server/app.js
+++ server/app.js
@@ -4,6 +4,23 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var allowCrossDomain = function(req, res, next) {
+  var origin = req.header('origin');
+  console.log('origin', origin);
+  if (   origin.indexOf('localhost') > -1
+      || origin.indexOf('ng-parth.xyz') > -1
+      || origin.indexOf('ng-parth') > -1
+      || origin.indexOf('heroku') > -1
+      || origin.indexOf('github') > -1) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/../'));
 app.use(require('body-parser').json());
 require('./api')(app);
