7ecd2f63b315fa02e3aae65d77deb790f1901aed
Parth <ppmistry204@gmail.com>
2024-04-25 16:15:10 +0530

    Add route tag facility.

--- server/api/letsgo/letsgo.controller.js
+++ server/api/letsgo/letsgo.controller.js
@@ -13,17 +13,27 @@ exports.getRouteTags = function (req, resp) {
     //   const rTags = ['Century => RGGC', 'Century => Kalanagar', "Century => Bharatnagar", "Century => WeW", "WeW => Kalanagar", "WeW => Century", "Kalanagar => Century"].map(t => new RouteTags({ id: nanoid(5), tagName: t}));
     //   RouteTags.insertMany(rTags).then(function(result) {
     //     resp.send({ action: 'success', data: result });
-    //   }).catch(err => handleError(err));
+    //   }).catch(err => handleError(resp, err));
     // } else
     resp.send({ action: 'success', data: tags });
   }).catch(err => handleError(resp, err));
 }
+
+exports.postRouteTag = function (req, resp) {
+  const {tagName} = req.body;
+  if (!tagName) return resp.send(400, {action: 'failure', error: 'Missing tag name'});
+  const newRouteTag = new RouteTags({id: nanoid(5), tagName})
+  RouteTags.create(newRouteTag)
+      .then(routeTag => resp.send({action: 'success', data: newRouteTag}))
+      .catch(err => handleError(resp, err));
+}
+
 exports.getRoutes = function (req, resp) {
   // console.log('getRoutes: ', req.params);
   const findQuery = { status: true, ...(req.params || {}) };
   Route.find(findQuery, {apiUrl: 0}).then(function(routes){
     resp.send({ action: 'success', data: routes });
-  }).catch(err => handleError(err));
+  }).catch(err => handleError(resp, err));
 }
 
 exports.getRouteStatus = function (req, resp) {
@@ -33,7 +43,7 @@ exports.getRouteStatus = function (req, resp) {
     RouteService.getRouteStatus(route)
         .then(routeStatus => resp.send({ action: 'success', data: routeStatus }))
         .catch(e => handleError(resp, e));
-  }).catch(err => handleError(err));
+  }).catch(err => handleError(resp, err));
 }
 
 exports.postRoute = function (req, resp) {
@@ -44,7 +54,7 @@ exports.postRoute = function (req, resp) {
       resp.send({action: 'success', data: newRoute});
   }).catch(err => {
     console.log('Err @postRoute: ', err);
-    return handleError(err);
+    return handleError(resp, err);
   })
 }
 
